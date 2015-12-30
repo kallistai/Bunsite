5b58b8b7227257cb6edeb0af73d0b67121c23837
Zach Pease <kallistai@gmail.com>
2015-12-30 14:23:18 -0500

    First commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Bunsite
+# bunsite
+# bunsite
---
+++ script.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+	
+	$("#addPlayer").click(function() {
+		var playerName = $("#player").val();
+		$("#players").append("<tr><td>" + playerName + "</td><td>Deck</td></tr>");
+	});
+});
