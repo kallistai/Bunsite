c472e5e572694f9321c9667e9a1a33b6e71a048c
kallistai <kallistai@gmail.com>
2016-01-23 11:27:18 -0500

    code updates from christmas break, sort of working submit page.

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ $(document).ready(function(){
 	
 	$("#addPlayer").click(function() {
 		var playerName = $("#player").val();
-		$("#players").append("<tr><td>" + playerName + "</td><td>Deck</td></tr>");
+		$("#players").append("<tr><td>" + playerName + "</td><td><input type='text' name='Commander' id='deck'></td><td><input type='button' value='Remove' class='removePlayer'></td></tr>");
+		$("#player").val('');
+	});
+	
+	$('#players').on('click', '.removePlayer', function(){
+   		$(this).closest('tr').remove();
 	});
 });
