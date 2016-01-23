7120fed351250aa6d43261d21e7d32dc79dbb82c
kallistai <kallistai@gmail.com>
2016-01-23 13:16:49 -0500

    Building the game tracker, after the setup page

--- script.js
+++ script.js
@@ -2,11 +2,29 @@ $(document).ready(function(){
 	
 	$("#addPlayer").click(function() {
 		var playerName = $("#player").val();
-		$("#players").append("<tr><td>" + playerName + "</td><td><input type='text' name='Commander' id='deck'></td><td><input type='button' value='Remove' class='removePlayer'></td></tr>");
+		var commanderName = $("#commander").val();
+		$("#players").append("<tr><td>" + playerName + "</td><td>" + commanderName + "</td><td><input type='button' value='Remove' class='removePlayer'></td></tr>");
 		$("#player").val('');
+		$("#commander").val('');
 	});
 	
 	$('#players').on('click', '.removePlayer', function(){
    		$(this).closest('tr').remove();
+	});
+
+	$('#gameStart').click(function(){
+		var myTableArray = [];
+
+$("table#players tr").each(function() {
+    var arrayOfThisRow = [];
+    var tableData = $(this).find('td');
+    if (tableData.length > 0) {
+        tableData.each(function() { arrayOfThisRow.push($(this).text()); });
+        myTableArray.push(arrayOfThisRow);
+    }
+});
+
+alert(myTableArray);
+		window.location.href = "gametracker.html";
 	});
 });
