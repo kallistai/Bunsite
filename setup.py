119300e4d11f67c4335d6f2c165c05e854f52d7c
JIMMY JIMMY JIMMY <DEAAANNNNN>
2016-02-16 16:43:31 -0500

    django files

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+        name='Bunsite',
+        version='',
+        packages=['Bunsite', 'gamelog', 'gamelog.migrations'],
+        url='',
+        license='',
+        author='Toddley',
+        author_email='',
+        description=''
+)
